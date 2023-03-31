8199925d559f4d79f6281ffa8371ad079e6d7c25
lunainfotec <kirtimadan1997@gmail.com>
2023-03-31 11:27:06 +0530

    added : code to print Wellcome Message

---
+++ src/com/bl/assigement1/WellcomeMeassge.java
@@ -0,0 +1,8 @@
+package com.bl.assigement1;
+
+public class WellcomeMeassge {
+
+    public static void main(String[] args) {
+        System.out.println(" Welcome to EmployeeWage Computation Program");
+    }
+}
